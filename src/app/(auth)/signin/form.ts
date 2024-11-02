f84e2c01def63d889271f73bb3a041228b4b8676
Daniil Fomenko <danil.fomchik@gmail.com>
2024-11-02 22:06:15 +0200

    deleted files

--- src/app/(auth)/signin/form.ts
+++
@@ -1,9 +0,0 @@
-import {object, string} from 'yup';
-
-export const defaultValues = {
-    newName: '',
-};
-
-export const validation = object().shape({
-    newName: string().required('New name is required'),
-});
