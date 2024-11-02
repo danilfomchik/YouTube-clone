ce724eaa654c852b29fde3b25a35b52d0307a212
Daniil Fomenko <danil.fomchik@gmail.com>
2024-11-02 22:13:17 +0200

    added hook for working with params

---
+++ src/app/services/hooks/useChangeParams.ts
@@ -0,0 +1,32 @@
+import {useSearchParams, useRouter, usePathname} from 'next/navigation';
+import {useCallback, useMemo} from 'react';
+
+import {SearchParamsKeys} from '../types';
+
+const redirectSearchKeys = [SearchParamsKeys.searchKey];
+
+export const useChangeParams = () => {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const {replace} = useRouter();
+
+    const params = useMemo(() => new URLSearchParams(searchParams as unknown as string), [searchParams]);
+
+    const addParams = useCallback(
+        ([key, value]: [SearchParamsKeys, string]) => {
+            params.set(key, value);
+            replace(`${redirectSearchKeys.includes(key) ? '/' : pathname}?${params.toString()}`);
+        },
+        [params, pathname, replace],
+    );
+
+    const deleteParams = useCallback(
+        (key: string) => {
+            params.delete(key);
+            replace(`${pathname}?${params.toString()}`);
+        },
+        [params, pathname, replace],
+    );
+
+    return {addParams, deleteParams, searchParams};
+};
