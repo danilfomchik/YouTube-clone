da3b51cb10c0404716ea238aff06ee118cf5ef63
Daniil Fomenko <danil.fomchik@gmail.com>
2024-11-02 22:04:49 +0200

    setup changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,34 @@
 import {NextRequest, NextResponse} from 'next/server';
 
-const protectedRoutes = ['/profile', '/add-pet', '/messages'];
-export const authRoutes = ['/signin', '/signup'];
+import {AuthSearchParamsValues} from './app/components/Buttons/LoginButton/types';
+import {SearchParamsKeys, StorageKeys} from './app/services/types';
+
+const protectedRoutes = ['/profile'];
+export const authRoutes = [
+    `?${SearchParamsKeys.authKey}=${AuthSearchParamsValues.signUpValue}`,
+    `?${SearchParamsKeys.authKey}=${AuthSearchParamsValues.signInValue}`,
+];
 
 export default function middleware(request: NextRequest) {
-    const userToken = request.cookies.get('userToken')?.value;
+    const userId = request.cookies.get(StorageKeys.userId)?.value;
     const path = request.nextUrl.pathname;
+    const search = request.nextUrl.search;
 
     const isProtectedRoute = protectedRoutes.includes(path);
-    const isAuthRoute = authRoutes.includes(path);
+    const isAuthRoute = authRoutes.includes(search);
 
-    if (isProtectedRoute && !userToken) {
-        request.cookies.delete('userToken');
+    if (isProtectedRoute && !userId) {
+        request.cookies.delete(StorageKeys.userId);
 
-        const response = NextResponse.redirect(new URL('/signin', request.url));
-        response.cookies.delete('userToken');
+        const response = NextResponse.redirect(
+            new URL(`/?${SearchParamsKeys.authKey}=${AuthSearchParamsValues.signInValue}`, request.url),
+        );
+        response.cookies.delete(StorageKeys.userId);
 
         return response;
     }
 
-    if (isAuthRoute && userToken) {
+    if (isAuthRoute && userId) {
         return NextResponse.redirect(new URL('/profile', request.url));
     }
 }
